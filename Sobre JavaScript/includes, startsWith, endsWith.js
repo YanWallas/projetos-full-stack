43275c659c0e98d14ea72d6f5e936caa0f6fd227
Yan Wallas <yanwallas98@gmail.com>
2025-02-24 16:42:26 -0300

    Create includes, startsWith, endsWith.js

---
+++ Sobre JavaScript/includes, startsWith, endsWith.js
@@ -0,0 +1,20 @@
+
+// includes - determina se um array contém um determinado elemento, retornando true ou false apropriadamente.
+// startsWith - verifica se uma string começa com um determinado valor.
+// endsWith - verifica se uma string termina com um determinado valor.
+
+// let nomes = ["Matheus", "Lucas", "Jose"];
+
+// //console.log(nomes.includes("Maria"));
+
+// if(nomes.includes("Matheus")){
+//   console.log("ESTA NA LISTA")
+// }else{
+//   console.log("NAO ESTA NA LISTA")
+// }
+
+let nome = "Matheus";
+
+//console.log(nome.startsWith("Mat"))
+
+console.log(nome.endsWith("eus"))
