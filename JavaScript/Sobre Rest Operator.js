a23dd496e473d1709212941f5993608488a4606b
Yan Wallas <yanwallas98@gmail.com>
2025-02-24 16:04:00 -0300

    Create Sobre Rest Operator.js

---
+++ JavaScript/Sobre Rest Operator.js
@@ -0,0 +1,21 @@
+
+// REST Operator é um operador do JavaScript que permite reunir os elementos restantes de um array em um novo array.
+
+// function convidados(...nomes){
+//   console.log("SEJA BEM VINDO TODOS CONVIDADOS")
+//   console.log(nomes);
+// }
+
+// convidados("Matheus", "Lucas", "Maria", "Ana caroline")
+
+
+function sorteador(...numeros){
+  console.log(numeros);
+
+  const numeroGerado = Math.floor(Math.random() * numeros.length);
+
+  console.log("Numero gerado foi: " + numeros[numeroGerado]);
+
+}
+
+sorteador(1, 4, 5, 15, 25, 90, 55, 34)
