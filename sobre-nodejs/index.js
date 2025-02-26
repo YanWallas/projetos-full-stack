56e72bb2df9e35355854f6de55be717841eb5962
Yan Wallas <yanwallas98@gmail.com>
2025-02-26 16:06:43 -0300

    Aplicando Middleware

--- sobre-nodejs/index.js
+++ sobre-nodejs/index.js
@@ -9,16 +9,43 @@ server.use(express.json());
 
 const cursos = ['Node JS', 'JavaScript', 'React native'];
 
+// Middleware Global
+server.use((req, res, next) => {
+  console.log(`URL CHAMADA: ${req.url}`);
+
+  return next();
+});
+
+// Função middleware que verifica se no post a requisição realmente mandou um nome. 
+function checkCurso(req, res, next){
+  if(!req.body.name){
+    return res.status(400).json({ error: "Nome do curso é obrigatorio"});
+  }
+
+  return next();
+}
+
+// Função middleware que verifica se no get a requisição realmente mandou um curso existente. 
+function checkIndexCurso(req, res, next){
+  const curso = cursos[req.params.index];
+
+  if(!curso){
+    return res.status(400).json({ error: "O curso não existe"});
+  }
+
+  req.curso = curso;
+
+  return next();
+}
+
 // Listando todos os cursos
 server.get('/cursos', (req, res) => {
   return res.json(cursos);
 })
 
 // Route params = /curso/2 -  retornando um curso
-server.get('/curso/:index', (req, res) => {
-  
-  const { index } = req.params;
-  return res.json(cursos[index]);
+server.get('/curso/:index', checkIndexCurso, (req, res) => {
+  return res.json(req.curso);
 
 })
 
@@ -31,7 +58,7 @@ server.get('/curso/:index', (req, res) => {
  })*/
 
 // Criando um novo curso.    
-server.post('/cursos', (req, res) => {
+server.post('/cursos', checkCurso, (req, res) => {
   const { name } = req.body;
   cursos.push(name);
 
@@ -39,7 +66,7 @@ server.post('/cursos', (req, res) => {
 });
 
 // Atualizando um curso
-server.put('/cursos/:index', (req, res) => {
+server.put('/cursos/:index', checkCurso, checkIndexCurso, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -49,7 +76,7 @@ server.put('/cursos/:index', (req, res) => {
 });
 
 // Excluindo algum curso
-server.delete('/cursos/:index', (req, res) => {
+server.delete('/cursos/:index', checkIndexCurso, (req, res) => {
   const { index } = req.params;
 
   //splice - adiciona ou remove objeto dentro de um array.
