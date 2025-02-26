6f4199186b5185abc364ed305a0fe6908f64c1a8
Yan Wallas <yanwallas98@gmail.com>
2025-02-26 15:46:54 -0300

    Criando route (CRUD) utilizando postman

--- sobre-nodejs/index.js
+++ sobre-nodejs/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Request Body = { nome: 'Nodejs', tipo: 'Backend' }
+// CRUD = Create, Read, Update, Delete.
 
 const cursos = ['Node JS', 'JavaScript', 'React native'];
 
-// Route params = /curso/2
+// Listando todos os cursos
+server.get('/cursos', (req, res) => {
+  return res.json(cursos);
+})
+
+// Route params = /curso/2 -  retornando um curso
 server.get('/curso/:index', (req, res) => {
   
   const { index } = req.params;
@@ -21,6 +29,34 @@ server.get('/curso/:index', (req, res) => {
 
     return res.json({ curso: `Aprendendo ${nome}`});
  })*/
+
+// Criando um novo curso.    
+server.post('/cursos', (req, res) => {
+  const { name } = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+// Atualizando um curso
+server.put('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos);
+});
+
+// Excluindo algum curso
+server.delete('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+
+  //splice - adiciona ou remove objeto dentro de um array.
+  cursos.splice(index, 1);
+  return res.json(cursos);
+});
+
 
 // Indicando o localhost
 server.listen(3000);
