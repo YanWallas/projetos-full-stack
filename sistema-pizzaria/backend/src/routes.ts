5af8a51b9a4bf34ba269e729f81788b1fdff1167
Yan Wallas <yanwallas98@gmail.com>
2025-02-27 15:49:55 -0300

    Config de tratativas de erros

--- sistema-pizzaria/backend/src/routes.ts
+++ sistema-pizzaria/backend/src/routes.ts
@@ -4,6 +4,9 @@ const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
   return res.json({ nome: "Sistema Pizzaria" })
+
+
+  //throw new Error('Erro ao fazer essa requisição') - simulação de erro
 })
 
 export { router };
--- sistema-pizzaria/backend/src/server.ts
+++ sistema-pizzaria/backend/src/server.ts
@@ -1,4 +1,7 @@
 import  express, { Request, Response, NextFunction } from "express";
+import 'express-async-errors';
+import cors from 'cors';
+
 import { router } from "./routes";
 
 const app = express();
@@ -6,8 +9,26 @@ const app = express();
 // Indicando que vai usar esse tipo de formato.
 app.use(express.json());
 
+// Pacote para habilitar qualquer URL/IP conseguir fazer requisição nessa API.
+app.use(cors());
+
 // vai utilizar essa route
 app.use(router);
+
+// Tratando erros 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    //se for uma instancia do tipo error
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.'
+  })
+})
 
 // porta do servidor, e mensagem quando der certo.
 app.listen(3333, () => console.log('Servidor online!'))
