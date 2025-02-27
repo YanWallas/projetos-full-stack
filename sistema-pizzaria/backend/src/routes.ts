07cfd18a874776c106b55981a054f2369bb2be70
Yan Wallas <yanwallas98@gmail.com>
2025-02-27 17:36:02 -0300

    config estrutura

--- sistema-pizzaria/backend/src/routes.ts
+++ sistema-pizzaria/backend/src/routes.ts
@@ -1,12 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-  return res.json({ nome: "Sistema Pizzaria" })
-
-
-  //throw new Error('Erro ao fazer essa requisição') - simulação de erro
-})
+// -- ROTAS USER --
+router.post('/users', new CreateUserController().handle)
 
 export { router };
