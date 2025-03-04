a2c0b75be1b8fffce006f29ad7e7f88da6af6420
Yan Wallas <yanwallas98@gmail.com>
2025-03-04 13:50:05 -0300

    Cadastrando User

--- sistema-pizzaria/backend/src/services/user/CreateUserService.ts
+++ sistema-pizzaria/backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from "../../prisma";
 
 interface UserRequest{
   name: string,
@@ -7,8 +8,39 @@ interface UserRequest{
 
 class CreateUserService{
   async execute({name, email, password}: UserRequest){
-    console.log(name);
-    return { name: name }
+    
+    // verificar se ele enviou um email 
+    if(!email){
+      throw new Error("Email incorrect")
+    }
+
+    //Verificar se esse email já está cadastrado na plataforma.
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    // Se ja existir o email cadastrado no banco. 
+    if(userAlreadyExists){
+      throw new Error("User already exists")
+    }
+
+    const user = await prismaClient.user.create({
+      data:{
+        name: name,
+        email: email,
+        password: password,
+      },
+      select:{
+        id: true,
+        name: true,
+        email: true,
+      }
+    })
+
+
+    return user;
   }
 }
 
