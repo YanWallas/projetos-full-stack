aab64575086da2617de752ca9f2748b04ad47494
Yan Wallas <yanwallas98@gmail.com>
2025-03-07 12:43:03 -0300

    Criando o delete de um pedido

--- sistema-pizzaria/backend/src/routes.ts
+++ sistema-pizzaria/backend/src/routes.ts
@@ -13,6 +13,7 @@ import { CreateProductController } from "./controllers/product/CreateProductCont
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -45,5 +46,8 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 
 // -- ROTAS ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
+
+// -- ROTA PARA DELETAR PEDIDO
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router };
