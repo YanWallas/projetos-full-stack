a7331d22550975e0f3b5b4aea1bb2fb3341aab0e
Yan Wallas <yanwallas98@gmail.com>
2025-03-09 13:03:24 -0300

    entendendo estrutura nextJs

---
+++ sobre-nextjs/meu-projeto/src/app/contatos/page.tsx
@@ -0,0 +1,8 @@
+export default function Contatos(){
+  return(
+    <div>
+      <h1>Página de Contatos</h1>
+      <span>Telefone: (62) 654616-65184</span>
+    </div>
+  )
+}
---
+++ sobre-nextjs/meu-projeto/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Página home</h1>
+      <span>Seja bem vindo a página home</span>
+    </main>
+  );
+}
