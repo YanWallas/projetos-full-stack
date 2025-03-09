49f15b23e188293d3e70053fcc43f4925c80144f
Yan Wallas <yanwallas98@gmail.com>
2025-03-09 13:36:29 -0300

    Server Component

--- sobre-nextjs/meu-projeto/src/app/page.tsx
+++ sobre-nextjs/meu-projeto/src/app/page.tsx
@@ -1,11 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+interface DataProps{
+  id: number;
+  name: string;
+  full_name: string;
+  owner:{
+    login: string;
+    id: number;
+    avatar_url: string;
+    url: string;
+  }
+}
+
+async function getData(){
+  //https://api.github.com/users/YanWallas/repos
+  const response = await fetch("https://api.github.com/users/YanWallas/repos")
+
+  return response.json();
+}
+
+export default async function Home() {
+  const data: DataProps[] = await getData();
+
   return (
     <main>
       <h1>Página home</h1>
       <span>Seja bem vindo a página home</span>
+
+      <h3>Meus repositorios</h3>
+      {data.map((item) => (
+        <div key={item.id}>
+          <strong>Repositorio: </strong> <a>{item.name}</a>
+          <br/><hr/>
+        </div>
+      ))}
     </main>
   );
 }
