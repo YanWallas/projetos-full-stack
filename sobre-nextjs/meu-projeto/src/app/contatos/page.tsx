0ee50a7441e8cc2f98ab280c58f84770be7e7e85
Yan Wallas <yanwallas98@gmail.com>
2025-03-10 17:17:35 -0300

    Criando grupos de rotas

--- sobre-nextjs/meu-projeto/src/app/contatos/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Contatos(){
-  return(
-    <div>
-      <h1>Página de Contatos</h1>
-      <span>Telefone: (62) 654616-65184</span>
-    </div>
-  )
-}
--- sobre-nextjs/meu-projeto/src/app/dashboard/cadastro/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Cadastro(){
-  return(
-    <div>
-      <h1>Cadastrando clientes</h1>
-      <p>Essa é a pagina de cadastro de clientes do dashboard</p>
-    </div>
-  )
-}
