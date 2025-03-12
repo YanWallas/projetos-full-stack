2f7c2a2d544a81c6baf9544e2f7a2d49761ce127
Yan Wallas <yanwallas98@gmail.com>
2025-03-12 16:03:24 -0300

    Criando pagina signup

--- sistema-pizzaria/frontend/src/app/signup/page.tsx
+++ sistema-pizzaria/frontend/src/app/signup/page.tsx
@@ -1,12 +1,53 @@
+import styles from '../page.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+import logoImg from '/public/logo.svg'
 
 export default function Signup(){
   return(
     <>
-      <main>
-        <h1>pagina cadastro
-          
-        </h1>
-      </main>
+     <div className={styles.containerCenter}>
+        <Image
+          src={logoImg}
+          alt='logo da pizzaria'
+        />
+
+        <section className={styles.login}>
+          <h1>Criando sua conta</h1>
+
+          <form>
+            <input 
+              type="text" 
+              required
+              name="name"
+              placeholder="Digite seu nome..."
+              className={styles.input}
+            />
+
+            <input 
+              type="email" 
+              required
+              name="email"
+              placeholder="Digite seu email..."
+              className={styles.input}
+            />
+
+            <input 
+              type="password" 
+              required
+              name="password"
+              placeholder="********"
+              className={styles.input}
+            />
+
+            <button type='submit' className={styles.button}>Cadastrar</button>
+          </form>
+
+          <Link href="/" className={styles.text}>
+            Já possui uma conta? Faça login
+          </Link>
+        </section>
+      </div>
     </>
   )
 }
