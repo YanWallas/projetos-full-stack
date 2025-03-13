eda1a453e643a3cd2f11f607c9e4067e49911456
Yan Wallas <yanwallas98@gmail.com>
2025-03-13 16:05:35 -0300

    Criando header dashboard

---
+++ sistema-pizzaria/frontend/src/app/dashboard/components/header/index.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import styles from './styles.module.scss';
+import Link from 'next/link';
+import logoImg from '/public/logo.svg';
+import { LogOutIcon } from 'lucide-react';
+
+export function Header(){
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/dashboard">
+          <Image
+            alt='Logo Slice Pizza'
+            src={logoImg}
+            width={190}
+            height={60}
+            priority={true}
+            quality={100}
+          />
+        </Link>
+
+        <nav>
+          <Link href='/dashboard/category'>
+            Categorias
+          </Link>
+          <Link href="/dashboard/product">
+            Produto
+          </Link>
+
+          <form>
+            <button type='submit'>
+              <LogOutIcon size={24} color='#FFF'/>
+            </button>
+          </form>
+        </nav>
+      </div>
+    </header>
+  )
+}
