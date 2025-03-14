cce5f898b54d12a2adee15319962204fc2e7a56b
Yan Wallas <yanwallas98@gmail.com>
2025-03-14 15:38:28 -0300

    ajustes de cores

--- sistema-pizzaria/frontend/src/app/dashboard/components/header/index.tsx
+++ sistema-pizzaria/frontend/src/app/dashboard/components/header/index.tsx
@@ -42,7 +42,7 @@ export function Header(){
 
           <form action={handleLogout}>
             <button type='submit'>
-              <LogOutIcon size={24} color='#FFF'/>
+              <LogOutIcon size={24}/>
             </button>
           </form>
         </nav>
