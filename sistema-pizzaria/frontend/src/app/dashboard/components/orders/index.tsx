c6e1ee2164b92fe2d84576888e941dce7e02450d
Yan Wallas <yanwallas98@gmail.com>
2025-03-18 19:16:25 -0300

    ajustando dashboard

--- sistema-pizzaria/frontend/src/app/dashboard/components/orders/index.tsx
+++ sistema-pizzaria/frontend/src/app/dashboard/components/orders/index.tsx
@@ -5,6 +5,8 @@ import { OrderProps } from '@/lib/order.type';
 import { Modaloarder } from '@/app/dashboard/components/modal';
 import { use } from 'react'
 import { OrderContext } from '@/providers/order';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 interface Props{
   orders: OrderProps[]
@@ -12,10 +14,17 @@ interface Props{
 
 export default function Orders({ orders }: Props){
   const { isOpen, onRequestOpren } = use(OrderContext)
+  const router = useRouter();
 
   async function handleDetailOrder(order_id: string){
     await onRequestOpren(order_id)
   }
+
+  function handleRefresh(){
+    router.refresh();
+    toast.success("Pedidos atualizados com sucesso!")
+  }
+
 
   return(
     <>
@@ -23,12 +32,18 @@ export default function Orders({ orders }: Props){
 
         <section className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
-          <button>
+          <button onClick={handleRefresh}>
             <RefreshCw size={24} color='#3fffa3'/>
           </button>
         </section>
 
         <section className={styles.listOrders}>
+          {orders.length === 0 && (
+            <span className={styles.emptyItem}>
+              Nenhum pedido aberto no momento...
+            </span>
+          )}
+
           {orders.map(order => (
             <button key={order.id} className={styles.orderItem} onClick={() => handleDetailOrder(order.id)}>
             <div className={styles.tag}></div>
