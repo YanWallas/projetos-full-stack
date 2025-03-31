f1780551306866d0df32da0a80077887cb8f2be7
Yan Wallas <yanwallas98@gmail.com>
2025-03-31 16:16:11 -0300

    aplicando estilos

--- react-native/tarefas/App.js
+++ react-native/tarefas/App.js
@@ -1,11 +1,47 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useState} from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [nome, setNome] = useState("Wallas");
+
+  function handleMudaNome(){
+    setNome("Você clicou no botão!");
+  }
+
   return(
-    <View style={{ flex: 1, backgroundColor: '#121212', justifyContent: "center", alignItems: "center" }}>
-      <Text style={{fontSize: 28, color: "#FFF"}}>Wallas</Text>
-      <Text style={{fontSize: 28, color: "#FFF"}}>Hello Word!!!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{nome}</Text>
+      
+      <TouchableOpacity style={styles.button} onPress={handleMudaNome}>
+        <Text style={styles.buttonText}>Mudar Nome</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "#121212",
+    justifyContent: "center",
+    padding: 28,
+  },
+  title:{
+    fontSize: 28,
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button:{
+    backgroundColor: 'red',
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonText:{
+    color: "#FFF",
+    fontSize: 18,
+    fontWeight: "bold",
+  }
+});
